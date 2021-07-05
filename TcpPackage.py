6d8f2d00199261b5f9f5854f5f2136fdc3082a57
hyong <hyong_cs@outlook.com>
2021-07-05 20:19:50 +0800

    休整不需要的属性

--- TcpPackage.py
+++ TcpPackage.py
@@ -12,18 +12,9 @@
 
 def get_tcp_package():
     return {
-        'src_port': 0, 'dest_port': 0,
+        'src_port': 0, 'dest_port': 0, 'seq': 0,
 
-        'seq': 0,
+        'ACK': 0, 'RST': 0, 'SYN': 0, 'ack': 0,
 
-        'check_num': 0,
-
-        'data_bias': 0, 'not_use': 0, 'URG': 0,
-        'ACK': 0, 'PSH': 0, 'RST': 0, 'SYN': 0,
-        'FIN': 0, 'windows': 0, 'ack': 0,
-
-        'check_sum': 0, 'urg_point': 0,
-
-        'option': 0, 'space': 0,
         'content': None, 'src_ip': None
     }
